a0dacc94b64d11d1c68bada6d1422da16bb0866a
Alex Giorgio Asole <alexgiorgio.asole@gmail.com>
2021-02-03 12:15:11 +0100

    aggiunti contatti per testare filter e scroll

--- js/script.js
+++ js/script.js
@@ -120,6 +120,92 @@ new Vue({
           }
         ],
       },
+      {
+        name: 'Luigi',
+        avatar: '_6',
+        visible: true,
+        messages: [
+          {
+            date: '20/01/2021 15:30:00',
+            text: 'Luigi Time! Let\'s go! ',
+            status: 'received'
+          },
+          {
+            date: '20/01/2021 15:40:25',
+            text: '2 anni che ci conosciamo e continui a fare la stessa battuta, manco avessi i baffi!',
+            status: 'sent'
+          },
+          {
+            date: '20/01/2021 15:41:00',
+            text: 'Meglio WaLuigi e Wario!',
+            status: 'sent'
+          },
+          {
+            date: '20/01/2021 16:00:30',
+            text: 'Ohi, non c\'è bisogno di offendere!',
+            status: 'received'
+          }
+        ],
+      },
+      {
+        name: 'Salomone',
+        avatar: '_7',
+        visible: true,
+        messages: [
+          {
+            date: '02/02/2021 10:30:00',
+            text: 'Ho bisogno di un consiglio',
+            status: 'sent'
+          },
+          {
+            date: '02/02/2021 10:35:25',
+            text: 'Dimmi',
+            status: 'received'
+          },
+          {
+            date: '02/02/2021 10:37:10',
+            text: 'Ho comprato un chilo di prosciutto perché era in offerta, non so che farmene però',
+            status: 'sent'
+          },
+          {
+            date: '02/02/2021 10:39:30',
+            text: 'Ci penso io, lo divido a metà e c\'è lo spartiamo',
+            status: 'received'
+          },
+          {
+            date: '02/02/2021 11:01:42',
+            text: 'Quanto sei saggio, sal',
+            status: 'sent'
+          }
+        ],
+      },
+      {
+        name: 'Marco',
+        avatar: '_8',
+        visible: true,
+        messages: [
+          {
+            date: '20/01/2021 15:30:00',
+            text: 'Ale, settimana prossima ci si vede con Andrea per decidere cosa fare con il premio della lotteria! ',
+            status: 'received'
+          },
+          {
+            date: '20/01/2021 15:40:25',
+            text: 'Non ci credo che abbiamo vinto centomila euro!',
+            status: 'sent'
+          },
+          {
+            date: '20/01/2021 15:41:00',
+            text: 'Infatti non è vero, questa chat non esiste , l\'hai creata come esercizio per Vuejs',
+            status: 'received'
+          },
+          {
+            date: '20/01/2021 16:00:30',
+            text: 'Ah, già, dimenticavo... vabbé',
+            status: 'sent'
+          }
+        ],
+      }
     ]
   },
   methods:{
