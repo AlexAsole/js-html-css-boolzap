5016a8e4c7b711500e2f0f6af5c478835dff62cc
Alex Giorgio Asole <alexgiorgio.asole@gmail.com>
2021-02-03 11:51:44 +0100

    funzioni tellTime e filterContacts

--- js/script.js
+++ js/script.js
@@ -5,10 +5,10 @@ new Vue({
     user:{
       name: 'Alex Giorgio',
       avatar: '_io',
-      visible: false,
       newMessage: '',
     },
     selectedContact: 0,
+    searchContact: '',
     contacts: [
       {
         name: 'Michele',
@@ -93,6 +93,33 @@ new Vue({
           }
         ],
       },
+      {
+        name: 'Faust',
+        avatar: '_5',
+        visible: true,
+        messages: [
+          {
+            date: '20/03/2020 16:30:00',
+            text: 'Fratm, n\'do stai? ',
+            status: 'received'
+          },
+          {
+            date: '20/03/2020 16:30:55',
+            text: 'Bella prete! Sto in Vaticano!',
+            status: 'sent'
+          },
+          {
+            date: '20/03/2020 16:35:00',
+            text: 'Ostia! Va che figata! La capella (XD) Sistina...',
+            status: 'received'
+          },
+          {
+            date: '20/03/2020 16:35:30',
+            text: 'Greve, zi, non abbiamo 12 anni',
+            status: 'sent'
+          }
+        ],
+      },
     ]
   },
   methods:{
@@ -116,21 +143,22 @@ new Vue({
       })
     },
     tellTime: function() {
-      let day = new Date().getDate();
-      let month = new Date().getMonth();
-      let year = new Date().getFullYear();
-      let hour = new Date().getHours();
-      let minute = new Date().getMinutes();
-      let second = new Date().getSeconds();
-      let time = `${day}/${month}/${year} ${hour}:${minute}:${second}`
-      return time
+      const date = dayjs().format('DD/MM/YYYY HH:mm:ss')
+      return date
     },
     lastConnection: function(i) {
       let lastIndex = this.contacts[i].messages.length - 1;
       let last = this.contacts[i].messages[lastIndex]
       if (last.status === 'received') {
         return last.date
-      } 
+      }
+    },
+    filterContacts: function(e) {
+      let searchName = this.searchContact.toLowerCase();
+      let filterName = e.name.toLowerCase();
+      if (filterName.startsWith(searchName)){
+        return true
+      }
     }
   }
 })
